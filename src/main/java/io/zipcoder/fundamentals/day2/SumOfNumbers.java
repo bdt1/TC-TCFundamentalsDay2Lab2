f9ffbe1ebb8170d7151ed355361725defeae3c1b
Thomas <O581051@NAEAST.AD.JPMORGANCHASE.com>
2017-06-26 01:06:35 -0400

    Completed Day2Lab2

--- src/main/java/io/zipcoder/fundamentals/day2/SumOfNumbers.java
+++ src/main/java/io/zipcoder/fundamentals/day2/SumOfNumbers.java
@@ -1,9 +1,15 @@
 package io.zipcoder.fundamentals.day2;
 
+import java.util.Scanner;
 
 public class SumOfNumbers {
 
     public static void main(String[] args){
-
+    	Scanner stdin = new Scanner(System.in);
+    	int n;  	
+    	System.out.print("Please enter a number to be added to 1: ");
+    	n = stdin.nextInt();
+    	System.out.print(1 + n);
+    	stdin.close();
     }
 }
